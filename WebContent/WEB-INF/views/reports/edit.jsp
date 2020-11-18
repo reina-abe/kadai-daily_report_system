d541f153c2b6128f26cd8461a8cc15c615652c5d
reina.abe <renabe07@yahoo.co.jp>
2020-11-18 18:22:11 +0900

    Add Report edit

---
+++ WebContent/WEB-INF/views/reports/edit.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:choose>
+            <c:when test="${report != null}">
+                <h2>日報　編集ページ</h2>
+                <form method="POST" action="<c:url value='/reports/update' />">
+                    <c:import url="_form.jsp" />
+                </form>
+            </c:when>
+            <c:otherwise>
+                <h2>お探しのデータは見つかりませんでした。</h2>
+            </c:otherwise>
+        </c:choose>
+
+        <p><a href="<c:url value='/reports/index' />">一覧に戻る</a></p>
+    </c:param>
+</c:import>
---
+++ src/controllers/reports/ReportsEditServlet.java
@@ -0,0 +1,44 @@
+package controllers.reports;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Report;
+import utils.DBUtil;
+
+@WebServlet("/reports/edit")
+public class ReportsEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public ReportsEditServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(r != null && login_employee.getId() == r.getEmployee().getId()) {
+        //ここで取得したidとDBで取得したidが同じなら
+            request.setAttribute("report", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", r.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
